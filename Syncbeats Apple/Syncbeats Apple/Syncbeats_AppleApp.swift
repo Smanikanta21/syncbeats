f5b4bad10e4e7e01a1ae6f2677f034d206b2f3ef
smanikanta21 <siraparapuabhinay21@gmail.com>
2026-07-07 18:53:22 +0530

    feat: initialize Android and Apple mobile clients and update server dependencies

---
+++ Syncbeats Apple/Syncbeats Apple/Syncbeats_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Syncbeats_AppleApp.swift
+//  Syncbeats Apple
+//
+//  Created by Abhinay Siraparapu on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Syncbeats_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
